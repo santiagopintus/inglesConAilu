c486dcc5dc3576eb0a3077072579d25c40ee14b5
Santiago Pintus <santiagopintus@gmail.com>
2021-08-04 11:27:02 -0300

    Ahora se mandan mails a través de Formspree.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,21 +17,31 @@ function toggleIconoHamburguesa() {
 
 function enviandoCorreos() {
     const formulario = document.querySelector('#formulario');
-    const mailtoButton = document.querySelector('#mailAilu');
+    // const mailtoButton = document.querySelector('#mailAilu');
 
     formulario.addEventListener('submit', handleSubmit);
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
         const formularioData = new FormData(formulario);
-        var nombreUsuario = formularioData.get('nombre');
-        var mensajeUsuario = formularioData.get('mensaje');
-        var telefonoUsuario = formularioData.get('telefono');
-        var emailUsuario = formularioData.get('email');
+        // var nombreUsuario = formularioData.get('nombre');
+        // var mensajeUsuario = formularioData.get('mensaje');
+        // var telefonoUsuario = formularioData.get('telefono');
+        // var emailUsuario = formularioData.get('email');
         
-        mailtoButton.setAttribute('href', `mailto:santiagopintus@gmail.com?subject=Consulta de ${nombreUsuario}&body=${mensajeUsuario} \nTeléfono: ${telefonoUsuario} \nE-mail: ${emailUsuario}`);
-        mailtoButton.setAttribute('target', '_blank');
-        mailtoButton.click();
-        
+        // mailtoButton.setAttribute('href', `mailto:santiagopintus@gmail.com?subject=Consulta de ${nombreUsuario}&body=${mensajeUsuario} \nTeléfono: ${telefonoUsuario} \nE-mail: ${emailUsuario}`);
+        // mailtoButton.setAttribute('target', '_blank');
+        // mailtoButton.click();
+        const response = await fetch(this.action, {
+            method: this.method,
+            body: formularioData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        if (response.ok) {
+            this.reset();
+            alert('El mensaje se envió correctamente.');
+        }
     }
 }
